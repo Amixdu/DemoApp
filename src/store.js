60b5bdce298a97188d10a42a5975cd6f92ca0503
Amindu Kaushal <amindu@surge.global>
2022-09-30 16:43:34 +0530

    Changed folder structure

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import accountReducer from "./features/account/accountSlice";
-
-export const store = configureStore({
-    reducer: {
-        account: accountReducer,
-    }
-})
